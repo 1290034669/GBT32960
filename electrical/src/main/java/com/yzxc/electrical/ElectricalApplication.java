0792215a71797b9b0e798cbee0200b83bb9b1e34
Arl <18337129068@163.com>
2023-03-26 12:56:06 +0800

    初始化项目

---
+++ electrical/src/main/java/com/yzxc/electrical/ElectricalApplication.java
@@ -0,0 +1,13 @@
+package com.yzxc.electrical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectricalApplication.class, args);
+    }
+
+}
---
+++ electrical/src/test/java/com/yzxc/electrical/ElectricalApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.yzxc.electrical;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ElectricalApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
